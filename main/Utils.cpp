8e1226e41fc7daa4c6dd2b755067895c92d5f13d
psychokiller <psychokiller1986@gmail.com>
2025-01-18 23:32:55 -0800

    Update Utils to CPP instead of C

---
+++ main/Utils.cpp
@@ -0,0 +1,70 @@
+#include "Utils.h"
+
+// I2C registers are 16 bits values (e.g. 0x8047) which means
+// that we can't hold its value in uint8_t anymore
+// this method will return an array of 2 uint8_t bytes
+// which can be used instead
+void convert_registers_to_byte_array(uint16_t slave_register, uint8_t *output)
+{
+    if (output != NULL)
+    {
+        output[0] = (uint8_t)(slave_register >> 8);   // MSB
+        output[1] = (uint8_t)(slave_register & 0xFF); // LSB
+    }
+}
+
+uint8_t I2C_WRITE(uint16_t register_address, uint8_t *write_buffer, uint8_t len, i2c_master_dev_handle_t *i2c_dev_handle)
+{
+    uint8_t wbuf[50] = {(uint8_t)((register_address >> 8) & 0xff), (uint8_t)(register_address & 0xff)};
+    for (uint8_t i = 0; i < len; i++)
+    {
+        wbuf[i + 2] = write_buffer[i];
+    }
+    return i2c_master_transmit(*i2c_dev_handle, wbuf, len + 2, -1); // send command
+}
+
+uint8_t I2C_READ(uint16_t register_address, uint8_t *read_buffer, uint8_t len, i2c_master_dev_handle_t *i2c_dev_handle)
+{
+    uint8_t *rbuf = read_buffer;
+    I2C_WRITE(register_address, 0, 0, i2c_dev_handle);
+
+    esp_err_t result = i2c_master_receive(*i2c_dev_handle, rbuf, len, -1);
+    // ESP_LOGI(TAG, "RESULT: %d", result);
+    return result;
+}
+
+void SPI_WRITE(uint8_t data, spi_device_handle_t handle)
+{
+    // ESP_LOGI("UTILS", "DATA %d", data);
+    gpio_set_level(SPI_CS, 0);
+    //  for (int i = 0; i < 8; i++)
+    // {
+    //     if ((data & 0x80) == 0) gpio_set_level(SPI_MOSI, 0);
+    //     else                    gpio_set_level(SPI_MOSI, 1);
+
+    //     data <<= 1;
+    //     gpio_set_level(SPI_CLK, 1);
+    //     gpio_set_level(SPI_CLK, 0);
+    // }
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));
+    
+    t.flags = SPI_TRANS_USE_TXDATA;
+    t.tx_buffer = NULL;
+    t.tx_data[0] = data;
+    t.length = 8;
+
+    // spi_device_polling_start(handle, &t, -1);
+    spi_device_polling_transmit(handle, &t);
+    // spi_device_polling_end(handle, -1);
+
+    gpio_set_level(SPI_CS, 1);
+}
+
+void SPI_WRITE_N(uint8_t *data, uint32_t len, spi_device_handle_t handle)
+{
+    for (uint32_t i = 0; i < len; i++)
+    {
+        SPI_WRITE(data[i], handle);
+    }
+}
--- main/Utils.h
+++ main/Utils.h
@@ -5,10 +5,16 @@
 #include "driver/i2c_master.h"
 #include "esp_system.h"
 #include "esp_check.h"
+#include "driver/spi_master.h"
+#include <string.h>
+#include "driver/gpio.h"
+#include "Config.h"
 
 void convert_registers_to_byte_array(uint16_t slave_register, uint8_t *output);
 
 uint8_t I2C_WRITE(uint16_t , uint8_t* , uint8_t, i2c_master_dev_handle_t*);
 uint8_t I2C_READ(uint16_t , uint8_t* , uint8_t , i2c_master_dev_handle_t*);
+void SPI_WRITE(uint8_t, spi_device_handle_t);
+void SPI_WRITE_N(uint8_t*, uint32_t, spi_device_handle_t);
 
 #endif
