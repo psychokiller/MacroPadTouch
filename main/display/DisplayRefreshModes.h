ce9b5b9d99f01345293fabeff1c9833591aa63f8
psychokiller <psychokiller1986@gmail.com>
2025-03-18 23:06:43 -0700

    Extract Display Refresh Modes

---
+++ main/display/DisplayRefreshModes.h
@@ -0,0 +1,11 @@
+#ifndef _DISPLAYREFRESHMODES_H_
+#define _DISPLAYREFRESHMODES_H_
+// Display refresh/redraw modes
+typedef enum
+{
+    FULL,
+    PARTIAL,
+    FAST
+} display_refresh_mode;
+
+#endif
